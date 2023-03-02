1971bbbc5e6572bdde360f422557548098b151d7
YazawaKenichi <tonfarken83825@gmail.com>
2023-03-02 22:16:44 +0900

    hoge

--- scripts/listener.py
+++ scripts/listener.py
@@ -7,7 +7,8 @@ from std_msgs.msg import String
 
 def callback(data):
     # Subscribed message are handed over in data
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    print(data.data, end = "\r\n")
+    # rospy.loginfo(data.data)
 
 def listener():
     # Node : listener
--- scripts/talker.py
+++ scripts/talker.py
@@ -4,18 +4,32 @@
 
 import rospy
 from std_msgs.msg import String
+import random
+
+def bar(num, bar_len):
+    ret = ""
+    for _ in range(num):
+        ret = ret + "#"
+    for _ in range(bar_len - num):
+        ret = ret + "-"
+    return ret
 
 def talker():
+    _max = 1000
+    bar_len = 50
     # Topic : chatter, Type : String, 
     pub = rospy.Publisher("chatter", String, queue_size = 10)
     # Node : talker
     rospy.init_node("talker", anonymous = True)
     # 10 Hz
-    r = rospy.Rate(10)
+    r = rospy.Rate(2)
     while not rospy.is_shutdown():
-        str = "Hello, World! %s"%rospy.get_time()
-        rospy.loginfo(str)
-        pub.publish(str)
+        num = random.randrange(_max - 1)
+        msg = f"{num:0>3} ["
+        msg = msg + bar(int(bar_len * num / _max), bar_len) + "]"
+        print(msg)
+        # rospy.loginfo(str)
+        pub.publish(msg)
         r.sleep()
 
 if __name__ == "__main__":
